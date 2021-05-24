8b1c8e005b5d853a214eec22e9b42376191f05e3
PurushothKumar1496 <84517635+PurushothKumar1496@users.noreply.github.com>
2021-05-24 12:18:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
-provider "azurerm"{
-  features{}
+provider "aws"{
+  region="eu-west-2"
+  access_key="AKIAYSP2ULZP5XFZYYPS"
+  secret_key="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"
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
-}
+# resource "azurerm_resource_group" "example" {
+#   name     = "example"
+#   location = "West Europe"
+# }
